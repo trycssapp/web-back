c0dc30b1fed15d9494614c40ae39ddb7a03750f5
Audun Hilden <audunhilden@live.no>
2023-01-04 12:04:12 +0100

    fix<search>: forgot to update endpoint

--- src/api/routes/search.ts
+++ src/api/routes/search.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { searchPages } from '../controllers/search/layouts/index';
+import { searchLayouts } from '../controllers/search/layouts/index';
 import { searchComponents } from './../controllers/search/components/index';
 
 const search = Router();
@@ -7,7 +7,7 @@ const search = Router();
 import { searchGlobal } from '../controllers/search/global';
 
 search.post('/', searchGlobal);
-search.post('/pages', searchPages);
+search.post('/layouts', searchLayouts);
 search.post('/components', searchComponents);
 
 export default search;
